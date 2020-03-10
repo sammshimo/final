0374a95cdbc0bcb65a6c9a1fb8a698414f88efbd
samshimo <sammshimo@gmail.com>
2020-03-10 16:40:29 -0700

    added reset button

--- map.js
+++ map.js
@@ -86,19 +86,36 @@ d3.json('asiageo.json').then(function(data) {
             plotData(pointData, '2/14/2020');
         });
 
-        document.getElementById('num-filter').disabled = true;
+        filter.append('button')
+            .text('Re-animate')
+            .style('margin', '20px')
+            .attr('id', 'reset')
+            .on('click', function(d) {
+                animate(pointData);
+            })
+        
+        animate(pointData);
 
-        let dates = ['1/21/2020', '1/22/2020', '1/23/2020', '1/24/2020', '1/25/2020', '1/26/2020', '1/27/2020',
-        '1/28/2020', '1/29/2020', '1/30/2020', '1/31/2020', '2/1/2020', '2/2/2020', '2/3/2020', '2/4/2020',
-        '2/5/2020', '2/6/2020', '2/7/2020', '2/8/2020', '2/9/2020', '2/10/2020', '2/11/2020', '2/12/2020',
-        '2/13/2020', '2/14/2020'];
+        // animates map
+        function animate(pointData) {
+            document.getElementById('num-filter').disabled = true;
+            document.getElementById('reset').disabled = true;
 
-        for (let i = 0; i < dates.length; i++) {
-            setTimeout(function() { plotData(pointData, dates[i]) }, 
-            1000*(i))
+            let dates = ['1/21/2020', '1/22/2020', '1/23/2020', '1/24/2020', '1/25/2020', '1/26/2020', '1/27/2020',
+            '1/28/2020', '1/29/2020', '1/30/2020', '1/31/2020', '2/1/2020', '2/2/2020', '2/3/2020', '2/4/2020',
+            '2/5/2020', '2/6/2020', '2/7/2020', '2/8/2020', '2/9/2020', '2/10/2020', '2/11/2020', '2/12/2020',
+            '2/13/2020', '2/14/2020'];
+
+            for (let i = 0; i < dates.length; i++) {
+                setTimeout(function() { plotData(pointData, dates[i]) }, 
+                1000*(i))
+            }
+
+            setTimeout(function () { 
+                document.getElementById('num-filter').disabled = false; 
+                document.getElementById('reset').disabled = false;
+            }, 1000 * dates.length);
         }
-
-        setTimeout(function () { document.getElementById('num-filter').disabled = false; }, 1000 * dates.length);
 
         // plots circles on the map
        function plotData(pointData, date) {
